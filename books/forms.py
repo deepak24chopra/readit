31e065abd78452a66fb15e78c69c80b5d16f8eb3
deepak chopra <deepak24chopra@gmail.com>
2017-03-11 20:05:18 +0530

    book save

--- books/forms.py
+++ books/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-
+from .models import Book
 
 
 class ReviewForm(forms.Form):
@@ -21,3 +21,9 @@ class ReviewForm(forms.Form):
 				'min_length': 'Please write atleast 300 characters (you have written %(show_value)s)'
 			}
 		)
+
+
+class BookForm(forms.ModelForm):
+	class Meta:
+		model = Book
+		fields = ['title','authors']
